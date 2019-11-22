0fb0bc792bdde1508bb1c62f26fe68d335914f22
Costeira <joaocurveira13@gmail.com>
2019-11-22 14:08:59 +0000

    update do parser

--- TP2/tp2/tp2_aco/parser.py
+++ TP2/tp2/tp2_aco/parser.py
@@ -12,18 +12,15 @@ def getFile():
 
 
 def getParagraph(corpus):
-    print("ACCESSING PARAGRAPHS")
     paragraphs=corpus.paras()
     return paragraphs
 
 def getWords(corpus):
-    print("ACCESSING WORDS")
     words=corpus.words()
     return words
 
 
 def getSentenses(corpus):
-    print("ACCESSING SENTENCES")
     sentences=corpus.sents()
     return sentences
 
@@ -36,55 +33,22 @@ def getTagger():
 stopwords = nltk.corpus.stopwords.words('portuguese')
 
 
+def filterLine(line):
+    return list(filter(lambda y: (y[1]=='V' or y[1]=='N' or y[1]=='NPROP') and (y[0] not in stopwords or y[1]=='V'), line))
+
+def filterMatrix(matrix):
+    return list(map(filterLine,matrix))
+
 
 def main():
-    print("Hello world")
+
     tagger = getTagger()
-    #tagged = tagger.tag(nltk.word_tokenize('Ontem, o João Antunes comeu peixe ao almoço'))
-
-    """
-
-        #Isto está a funcionar o problema é que o processamento é lento, por isso tenho a matrix poupa tempo.
-
     corpus=getFile()
-    sent_Matrix=getSentenses(corpus)
-    print(sent_Matrix)
+    sent_Matrix=getSentenses(corpus) #matrix, cada linha é uma frase.
 
-  
-    #gramas para obter componente gramatical
-    tagged_sents = nltk.corpus.mac_morpho.tagged_sents()
-    t0 = nltk.DefaultTagger('N')
-    t1 = nltk.UnigramTagger(tagged_sents, backoff=t0)
-
-    gramer_Matrix = list(map(t1.tag,sent_Matrix))
-
-    print(gramer_Matrix)
-    """
-    #print(poupar_Tempo)
-    #print(stopwords)
-
-    """
-    my_string=[('A', 'ART'), ('casa', 'N'), ('que', 'PRO-KS-REL'), ('os', 'ART'),
-             ('Maias', 'NPROP'), ('vieram', 'V'), ('habitar', 'N'), ('em', 'PREP|+'),
-             ('Lisboa', 'NPROP'), (',', ','), ('no', 'KC'), ('Outono', 'NPROP'), ('de', 'PREP'),
-             ('1875', 'N'), (',', ','), ('era', 'V'), ('conhecida', 'PCP'), ('na', 'NPROP'),
-             ('vizinhança', 'N'), ('da', 'NPROP'), ('Rua', 'NPROP'), ('de', 'PREP'), ('S', 'NPROP'),
-             ('.', '.'), ('Francisco', 'NPROP'), ('de', 'PREP'), ('Paula', 'NPROP'), (',', ','),
-             ('e', 'KC'), ('em', 'PREP|+'), ('todo', 'PROADJ'), ('o', 'ART'), ('bairro', 'N'),
-             ('das', 'NPROP'), ('Janelas', 'NPROP'), ('Verdes', 'NPROP'), (',', ','), ('pela', 'NPROP'),
-             ('Casa', 'NPROP'), ('do', 'NPROP'), ('Ramalhete', 'N'), (',', ','), ('ou', 'KC'),
-             ('simplesmente', 'ADV'), ('o', 'ART'), ('Ramalhete', 'N'), ('.', '.')]
-    """         
-    #result = [x.strip() for x in my_string.split(',')]
-    #line = [x for (x,y) in my_string]
-    #filtrado1 = list(filter(lambda y: (y[1]=='V' or y[1]=='N' or y[1]=='NPROP'), my_string))
-    #print(filtrado1)
-    #filtrado = list(filter(lambda y: (y[1]=='V' or y[1]=='N' or y[1]=='NPROP') and (y[0] not in stopwords or y[1]=='V'), my_string))
-    #print(filtrado)
-    #print(stopwords)
-
-    #print(line)
-
+    grammar_Matrix = list(map(tagger.tag,sent_Matrix)) #matix tuplos:(word,grammar)
+    filtered_gm=filterMatrix(grammar_Matrix)    #filtered: names and verbs only.
+    print(filtered_gm)
 
 if __name__ == "__main__":
     main()    
