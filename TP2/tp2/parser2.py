abfebb57458abe77815f97f391313067ead500aa
Costeira <joaocurveira13@gmail.com>
2019-11-22 12:41:23 +0000

    tp2/tp2aco é onde tem agora a parte aço do trabalho

--- TP2/tp2/parser2.py
+++ TP2/tp2/parser2.py
@@ -4,16 +4,19 @@ import nltk.corpus
 sentence = """Frase em portugues. Hoje comi bolinhos, estava muito bom."""
 
 tokens = nltk.word_tokenize(sentence)
+
 stopwords = nltk.corpus.stopwords.words('portuguese')
 
 print(tokens)
+
 
 tagged = nltk.pos_tag(tokens)
 
 #tagged2=nltk.corpus.mac_morpho.tagged_words()
 
-ola = nltk.corpus.mac_morpho.pos_tag(tokens)
+ola = nltk.corpus.mac_morpho.pos_tag()
 
 
 print(tagged)
 
+#cetem-publico
--- TP2/tp2/tp2_aco/parser.py
+++ TP2/tp2/tp2_aco/parser.py
@@ -1,12 +1,13 @@
 import nltk
 import sys 
+from pickle import load
+
 
 def getFile():
     try:
         corpus = nltk.corpus.reader.plaintext.PlaintextCorpusReader(".", "input.txt")
     except:
         sys.exit("Can't open input file")  
-
     return corpus
 
 
@@ -26,23 +27,21 @@ def getSentenses(corpus):
     sentences=corpus.sents()
     return sentences
 
-
-poupar_Tempo=[[('A', 'ART'), ('casa', 'N'), ('que', 'PRO-KS-REL'), ('os', 'ART'),
-             ('Maias', 'NPROP'), ('vieram', 'V'), ('habitar', 'N'), ('em', 'PREP|+'),
-             ('Lisboa', 'NPROP'), (',', ','), ('no', 'KC'), ('Outono', 'NPROP'), ('de', 'PREP'),
-             ('1875', 'N'), (',', ','), ('era', 'V'), ('conhecida', 'PCP'), ('na', 'NPROP'),
-             ('vizinhança', 'N'), ('da', 'NPROP'), ('Rua', 'NPROP'), ('de', 'PREP'), ('S', 'NPROP'),
-             ('.', '.'), ('Francisco', 'NPROP'), ('de', 'PREP'), ('Paula', 'NPROP'), (',', ','),
-             ('e', 'KC'), ('em', 'PREP|+'), ('todo', 'PROADJ'), ('o', 'ART'), ('bairro', 'N'),
-             ('das', 'NPROP'), ('Janelas', 'NPROP'), ('Verdes', 'NPROP'), (',', ','), ('pela', 'NPROP'),
-             ('Casa', 'NPROP'), ('do', 'NPROP'), ('Ramalhete', 'N'), (',', ','), ('ou', 'KC'),
-             ('simplesmente', 'ADV'), ('o', 'ART'), ('Ramalhete', 'N'), ('.', '.')]]
+def getTagger():
+    input = open('mac_morpho.pkl', 'rb')
+    tagger = load(input)
+    input.close()
+    return tagger
 
 stopwords = nltk.corpus.stopwords.words('portuguese')
 
 
 
 def main():
+    print("Hello world")
+    tagger = getTagger()
+    #tagged = tagger.tag(nltk.word_tokenize('Ontem, o João Antunes comeu peixe ao almoço'))
+
     """
 
         #Isto está a funcionar o problema é que o processamento é lento, por isso tenho a matrix poupa tempo.
@@ -64,6 +63,7 @@ def main():
     #print(poupar_Tempo)
     #print(stopwords)
 
+    """
     my_string=[('A', 'ART'), ('casa', 'N'), ('que', 'PRO-KS-REL'), ('os', 'ART'),
              ('Maias', 'NPROP'), ('vieram', 'V'), ('habitar', 'N'), ('em', 'PREP|+'),
              ('Lisboa', 'NPROP'), (',', ','), ('no', 'KC'), ('Outono', 'NPROP'), ('de', 'PREP'),
@@ -74,22 +74,16 @@ def main():
              ('das', 'NPROP'), ('Janelas', 'NPROP'), ('Verdes', 'NPROP'), (',', ','), ('pela', 'NPROP'),
              ('Casa', 'NPROP'), ('do', 'NPROP'), ('Ramalhete', 'N'), (',', ','), ('ou', 'KC'),
              ('simplesmente', 'ADV'), ('o', 'ART'), ('Ramalhete', 'N'), ('.', '.')]
+    """         
     #result = [x.strip() for x in my_string.split(',')]
     #line = [x for (x,y) in my_string]
     #filtrado1 = list(filter(lambda y: (y[1]=='V' or y[1]=='N' or y[1]=='NPROP'), my_string))
     #print(filtrado1)
-    filtrado = list(filter(lambda y: (y[1]=='V' or y[1]=='N' or y[1]=='NPROP') and (y[0] not in stopwords or y[1]=='V'), my_string))
-    print(filtrado)
+    #filtrado = list(filter(lambda y: (y[1]=='V' or y[1]=='N' or y[1]=='NPROP') and (y[0] not in stopwords or y[1]=='V'), my_string))
+    #print(filtrado)
     #print(stopwords)
 
     #print(line)
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
