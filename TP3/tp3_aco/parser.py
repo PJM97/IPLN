a16996c710a789e01e81d4ea8aeed2f7955828d5
Costeira <joaocurveira13@gmail.com>
2019-12-17 12:24:42 +0000

    coisas

--- TP3/tp3_aco/parser.py
+++ TP3/tp3_aco/parser.py
@@ -9,25 +9,30 @@ from collections import Counter
 import csv
 
 
-"""
-        _________________
-        |   Enunciado   |
-        |_______________|
-        
-Anotar o texto com linguakit, freeling, spacy, nltk, conll,...
-Dado o texto anotado e umas regras (Padrão, Ação) criar triplos de informação extraída
-Fazer um sistema de regras onde de alguma maneira pode-se dizer “quero procurar o padrão "Nome, ..."".
+r"""
+     __________________
+    |                  |
+    |Enunciado : Tema 1|
+    |__________________|    
+ Usar o nltk 
+Dado o texto anotado e umas regras (Padrão, Ação) criar triplos de informação extraída.
+ “quero procurar o padrão "Nome, ..."".
 Exemplo de padrão:
-
-grep -Po ’verde e \w+’ JornalAngolano.txt (ficheiro no natura jj)
-
-Output: verde e branco; verde e amarelo
+    grep -Po ’verde e \w+’ JornalAngolano.txt (ficheiro no natura jj)
+    Output: verde e branco; verde e amarelo
 
 Os triplos de informação extraída deverão estar relacionados com o texto e com padrão dado como input
-
 Exemplos de padrões: Adj NP ; Adj Adj N ; ...
-
 Mais avançado: Pode ser um adjetivo concreto ou um adjetivo qualquer (lemma)
+
+Exemplos:
+    De coisas a fazer:
+        Encontrar triplos relacionados por um dado verbo.
+        Encontrar todas as relações com um dado verbo especifico
+
+
+Podemos para uma palavra buscar a sua anterior e a sua seguinte.        
+
 """
 
 
@@ -40,6 +45,7 @@ def getALLFile():
         sys.exit("Can't open input file")  
     return corpus
 
+
 #Parse a specific file.
 def getFromOneFile(path,fname):
     try:
@@ -48,18 +54,21 @@ def getFromOneFile(path,fname):
         sys.exit("Can't open input file")   
     return corpus 
 
+#separa cada um dos paragrafos
 def getParagraph(corpus):
     paragraphs=corpus.paras()
     return paragraphs
 
+#separa cada uma das palavras
 def getWords(corpus):
     words=corpus.words()
     return words
-
+#separa cada uma das frases
 def getSentenses(corpus):
     sentences=corpus.sents()
     return sentences
 
+#get do tagger: Permite a identificação gramatical de cada uma das palavras.
 def getTagger():
     input = open('mac_morpho.pkl', 'rb')
     tagger = load(input)
@@ -69,11 +78,12 @@ def getTagger():
 #get all portuguese stopwords.
 stopwords = nltk.corpus.stopwords.words('portuguese')
 
-#filtrar nomes e verbos.
+#filtrar nomes e verbos de cada linha.
 def filterLine(line):
     return  list(filter(lambda y: (y[1]=='V' or y[1]=='N' or y[1]=='NPROP')
-             and (y[0] not in stopwords or y[1]=='V'), line)) #stopword que tb é verbo fica.
+             and (y[0] not in stopwords or y[1]=='V'), line)) #stopword inclui verbos em alguns casos.
 
+#Filtra sobre o texto apenas nomes e verbos
 def filterMatrix(matrix):
     return list(map(filterLine,matrix))
 
@@ -160,32 +170,48 @@ def wordRelation(word,filtered_gm):
 
 def main():
 
-    #get file and grammar
-    tagger = getTagger()
-
-    corpus=getFromOneFile(".","input.txt")
+    tagger = getTagger()     #obter o tagger gramatical
+    corpus=getFromOneFile(".","input.txt")  #ler input file
     #corpus=getALLFile()    #para dar parse de multiplos files.
-
     sent_Matrix=getSentenses(corpus) #matrix, cada linha é uma frase.
 
+    """
+    Importante pro trabalho:
+        Temos aqui a matrix com as palavras e a sua correspondente valor gramatical
+    """
     grammar_Matrix = list(map(tagger.tag,sent_Matrix)) #matix tuplos:(word,grammar)
+    #print(grammar_Matrix)
+
+
+
+   
     filtered_gm=filterMatrix(grammar_Matrix)    #filtered: names and verbs only.
+
+    #print(filtered_gm) #matriz filtrada pela segunda componente de cada elemento.
     
     #bi e tri-gramas.
     bigram=[]
     trigram=[]
     bigram , trigram = povoate_grams(filtered_gm)
-
+    """
+        Em Bigrams -> temos os pares de nomes consecutivos
+        Em Trigrams -> temos os triplos, onde o elem do meio é um verbo.
+    """
+    #print(trigram)
+    
     #print(bigram)
-    print(trigram)
+    #print(trigram)
 
     #separar os trigramas.
+    #Exemplo:('Maias', 'vieram','habitar') -> ('Maias', 'vieram'), ('vieram', 'habitar')
     tri_two=tritotow(trigram)
-    print(tri_two)
-
-    nomes_weight=weigthCounter(bigram)
-    verbs_weight=weigthCounter(tri_two)
+    #print(tri_two)
     
+    nomes_weight=weigthCounter(bigram)  #gera trigram com o weight na ultima componete
+    #print(nomes_weight)
+    
+    verbs_weight=weigthCounter(tri_two)
+    print(verbs_weight)
     #dicionario
     #res_dic=dic_names(bigram)
     #res_verb=dic_verbs(trigram)
